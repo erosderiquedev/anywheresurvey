775b42c7e20fb375efefaf41243620c6596b2912
Eros Derique Utui <erosderiquedev@gmail.com>
2025-03-03 15:41:16 +0200

    feat: participant

---
+++ src/main/java/com/erosutuidev/anywheresurvey/participant/Participant.java
@@ -0,0 +1,28 @@
+/**
+ * Author: Eros Derique Utui
+ * User:erosderiquedev
+ * Date:03/03/2025
+ * Time:15:21
+ */
+package com.erosutuidev.anywheresurvey.participant;
+
+import com.erosutuidev.anywheresurvey.survey.SurveyTemplate;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String identifier;
+
+    @ManyToOne
+    private SurveyTemplate surveyTemplate;
+}
