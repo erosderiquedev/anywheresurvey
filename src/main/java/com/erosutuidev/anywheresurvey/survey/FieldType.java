5d74dc707695de4ff0dee7cea30619a15e80d4c1
Eros Derique Utui <erosderiquedev@gmail.com>
2025-03-03 15:40:15 +0200

    feat: survey

---
+++ src/main/java/com/erosutuidev/anywheresurvey/survey/FieldType.java
@@ -0,0 +1,11 @@
+/**
+ * Author: Eros Derique Utui
+ * User:erosderiquedev
+ * Date:03/03/2025
+ * Time:15:20
+ */
+package com.erosutuidev.anywheresurvey.survey;
+
+public enum FieldType {
+    TEXT, NUMBER, DATE, TIME, DATETIME, BOOLEAN, INTEGER, DECIMAL, SELECT, IMAGE, LOCATION
+}
---
+++ src/main/java/com/erosutuidev/anywheresurvey/survey/SurveyField.java
@@ -0,0 +1,30 @@
+/**
+ * Author: Eros Derique Utui
+ * User:erosderiquedev
+ * Date:03/03/2025
+ * Time:15:20
+ */
+package com.erosutuidev.anywheresurvey.survey;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SurveyField {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fieldName;
+    private String label;
+
+    @Enumerated(EnumType.STRING)
+    private FieldType type; // TEXT, NUMBER, SELECT, IMAGE, LOCATION
+
+    @ManyToOne
+    private SurveyTemplate surveyTemplate;
+}
