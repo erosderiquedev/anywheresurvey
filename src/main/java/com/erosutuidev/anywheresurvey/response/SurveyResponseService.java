e1ec844eb55bf92407637294dc876a69f8981329
Eros Derique Utui <erosderiquedev@gmail.com>
2025-03-03 15:40:50 +0200

    feat: survey response

---
+++ src/main/java/com/erosutuidev/anywheresurvey/response/SurveyResponseService.java
@@ -0,0 +1,10 @@
+/**
+ * Author: Eros Derique Utui
+ * User:erosderiquedev
+ * Date:03/03/2025
+ * Time:15:35
+ */
+package com.erosutuidev.anywheresurvey.response;
+
+public class SurveyResponseService {
+}
