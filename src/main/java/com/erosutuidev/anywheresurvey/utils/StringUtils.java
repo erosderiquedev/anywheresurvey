ce40588eee8e927e6380ac3c10a342623ea3684a
Eros Derique Utui <erosderiquedev@gmail.com>
2025-03-03 12:59:37 +0200

    feat: StringUtils

---
+++ src/main/java/com/erosutuidev/anywheresurvey/utils/StringUtils.java
@@ -0,0 +1,30 @@
+package com.erosutuidev.anywheresurvey.utils;
+
+public class StringUtils {
+    private StringUtils(){} // Private constructor to prevent instantiation
+
+    public static boolean isNullOrEmpty(String str){
+        return str == null || str.trim().isEmpty();
+    }
+
+    public static String capitalize(String str) {
+        if (isNullOrEmpty(str)) {
+            return str;
+        }
+        return str.toUpperCase();
+    }
+
+    public static String toLowerCase(String str) {
+        if (isNullOrEmpty(str)) {
+            return str;
+        }
+        return str.toLowerCase();
+    }
+
+    public static String capitalizeFirstLetter(String str) {
+        if (isNullOrEmpty(str)) {
+            return str;
+        }
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+}
