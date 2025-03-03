d8a41414c6be936d5788ac5d0230f72a93235810
Eros Derique Utui <erosderiquedev@gmail.com>
2025-03-03 12:26:59 +0200

    Initial commit

---
+++ src/main/java/com/erosutuidev/anywheresurvey/AnywheresurveyApplication.java
@@ -0,0 +1,13 @@
+package com.erosutuidev.anywheresurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnywheresurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnywheresurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erosutuidev/anywheresurvey/AnywheresurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erosutuidev.anywheresurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnywheresurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
